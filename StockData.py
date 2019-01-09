5e07838214b0c2aac6dc476af0a34885899bf2f5
Stephen Minakian <stephenminakian@gmail.com>
2019-01-09 16:59:28 -0700

    Removed Notes

--- StockData.py
+++ StockData.py
@@ -22,57 +22,11 @@ class Stock_Data:
 
 
 
-# Add Comission to All Areas
-
-## Dashboard
-# Date
-# Symbol
-# Volume - Total for buy/sell
-# Executions
-# P&L
-
-## Per Trade Breakdown
-# Date/Time
-# Symbol
-# Quantity
-# Price
-# Position
+class TradeData:
+  date_time = ''
+  symbol = ''
+  quantity = int()
+  price = float()
+  position = int()
 
 
-
-##### Other #####
-# Long vs Short
-# Win/Loss
-# Win Rankings
-# Loss Rankings
-
-
-### Reports
-# Average Per-Trade Performance
-
-## Statistics ##
-# Total Gain/Loss
-# Average Daily Gain/Loss
-# Average Daily Volume
-# Average Winning Trade
-# Average Losing Trade
-# Total Trades
-# Number of Winning Trades
-# Number of Losing Trades
-# Max Consecutive Wins
-# Largest Gain
-# Largest Loss
-# Average Per-Share gain/loss
-# Average Trade Gain/Loss
-# Trade P&L Standard Deviation
-# Profit Factor
-# Average Hold Time (Winning Trades)
-# Average Hold Time (Losing Trades)
-# Max Consecutive Loses
-
-### Filtering ###
-# Symbol
-# Tag
-# Side (Long/Short)
-# Duration
-# Date Range
