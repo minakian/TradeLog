d019554549993c658956fa319095da3997e8c78d
Stephen Minakian <stephenminakian@gmail.com>
2019-01-09 16:02:38 -0700

    Making more modular, Added future feature list

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # TradeLog
+
+## Planned Features
+
+# Dashboard
+Date
+Symbol
+Volume - Total for buy/sell
+Executions
+P&L
+
+# Per Trade Breakdown
+Date/Time
+Symbol
+Quantity
+Price
+Position
+
+
+
+# Other
+Long vs Short
+Win/Loss
+Win Rankings
+Loss Rankings
+
+
+# Reports
+Average Per-Trade Performance
+
+## Statistics
+Total Gain/Loss
+Average Daily Gain/Loss
+Average Daily Volume
+Average Winning Trade
+Average Losing Trade
+Total Trades
+Number of Winning Trades
+Number of Losing Trades
+Max Consecutive Wins
+Largest Gain
+Largest Loss
+Average Per-Share gain/loss
+Average Trade Gain/Loss
+Trade P&L Standard Deviation
+Profit Factor
+Average Hold Time (Winning Trades)
+Average Hold Time (Losing Trades)
+Max Consecutive Loses
+
+## Filtering
+Symbol
+Tag
+Side (Long/Short)
+Duration
+Date Range
+
+* Add Comission to All Areas *
--- tradeLog.py
+++ tradeLog.py
@@ -4,35 +4,13 @@ import sys
 import os
 import matplotlib.pyplot as plt
 
-import StockData as SD
+import StockData as sd
+import DataImporting as di
 
 #nvda = SD.Stock_Data('NVDA', 1.0, 3, 5)
 
-# TODO: How can I read .csv files from a different folder
 # TODO: Use class to track specific trades - Traded multiple stocks at the same time created issues
-
-
-def flipData(filePath, data):
-  with open(filePath, mode='w', newline='') as textfile:
-    dest = csv.writer(textfile)
-    dest.writerow(data[0])
-    data = reversed(data[1:])
-    for row in data:
-      dest.writerow(row)
-
-def importData(filePath):
-  # Reverse the order of the data, but retain headder
-  reversed_data = list()
-  with open(filePath, newline='') as textfile:
-    source = csv.reader(textfile)
-    reversed_data = list(csv.reader(textfile))
-
-  # Verify data order, if correct don't flip
-  if(reversed_data[1][6] > reversed_data[-2][6]):
-    flipData(filePath, reversed_data)
-
-  data = pandas.read_csv(filePath, infer_datetime_format=['Time'])
-  return data
+# TODO: Track long/short bias, profit diff
 
 def trackTrades(data):
   income = float()
@@ -98,7 +76,7 @@ if __name__ == '__main__':
     for file in files:
        if file.endswith(".csv"):
          print(file)
-         data.append(importData(root + file))
+         data.append(di.importData(root + file))
 
   print(data)
   income, comission = float(), float()
@@ -131,3 +109,6 @@ if __name__ == '__main__':
 #       Trade
 #
 #   Total investment per trade
+#   Long vs Short:
+#     Bias
+#     Profit/Loss
